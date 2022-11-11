7f0ce3c9a148e57b4a4b1ee10bfb3b979f44e89d
Androo <84476053+Anduwu1@users.noreply.github.com>
2022-11-11 11:11:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,55 @@
 import requests
 import random
 
-CONTESTID = 1750
-r = requests.get(
-  f'https://codeforces.com/api/contest.status?contestId={CONTESTID}&count=250')
-file = r.json()
+sample = []
+c = []
+for i in range(3):
+  CONTESTID = 1750
+  r = requests.get(
+    f'https://codeforces.com/api/contest.status?contestId={CONTESTID}&count=250'
+  )
 
-passed = []
-i = 0
-while len(passed) < 249:
-  id = file['result'][i]['id']
-  mem = file['result'][i]['memoryConsumedBytes']
-  time = file['result'][i]['timeConsumedMillis']
-  contest = file['result'][i]['contestId']
-  i = +1
-  temp = {"id": id, "mem": mem, "time": time, "contestId": contest}
-  if (file['result'][i]['verdict'] == 'OK'):
-    passed.append(temp)
-passedstr = ""
-with open(f"{CONTESTID}-passed.csv", "w") as f:
-  passedstr = str(passed).replace("'", '"')
-  res = f"contest-id,time,mem\n"
-  for i in range(len(passed)):
+  file = r.json()
+
+  passed = []
+  i = 0
+  while len(passed) < 249:
     id = file['result'][i]['id']
     mem = file['result'][i]['memoryConsumedBytes']
     time = file['result'][i]['timeConsumedMillis']
     contest = file['result'][i]['contestId']
-    res += (f"{contest}-{id},{time},{mem}\n")
-  f.write(res)
+    i = +1
+    temp = {"id": id, "mem": mem, "time": time, "contestId": contest}
+    if (file['result'][i]['verdict'] == 'OK'):
+      passed.append(temp)
+  passedstr = ""
+  with open(f"{CONTESTID}-passed.csv", "w") as f:
+    passedstr = str(passed).replace("'", '"')
+    res = f"contest-id,time,mem\n"
+    for i in range(len(passed)):
+      id = file['result'][i]['id']
+      mem = file['result'][i]['memoryConsumedBytes']
+      time = file['result'][i]['timeConsumedMillis']
+      contest = file['result'][i]['contestId']
+      res += (f"{contest}-{id},{time},{mem}\n")
+    f.write(res)
 
-for i in range(149):
-  s = random.randint(0, 147)
-  passed[i]
+  for i in range(149):
+    s = random.randint(0, 147)
+    if s not in c:
+      sample.append(passed[s])
+      c.append(s)
+    else:
+      i -= 1
+
+print(sample)
+#with open(f"fullsample.csv", "w") as f:
+#  samplestr = str(sample).replace("'", '"')
+#  res = f"contest-id,time,mem\n"
+#  for i in range(len(sample)):
+#    id = sample[i]
+#    mem = file['result'][i]['memoryConsumedBytes']
+#    time = file['result'][i]['timeConsumedMillis']
+#    contest = file['result'][i]['contestId']
+#    res += (f"{contest}-{id},{time},{mem}\n")
+#  f.write(res)
